0f9656eb7ec17bff40e4e2b78764a26db0733a3b
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-09-01 16:50:44 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,9 +1,10 @@
 import os
+from os import getenv
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-SESSION_NAME = os.getenv("SESSION_NAME", "")
-AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "")
-VIDEO_THUMBNAIL = os.environ.get("VIDEO_THUMBNAIL", "")
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "TGbotsXD")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+SESSION_NAME = getenv("SESSION_NAME", "")
+AUDIO_THUMBNAIL = environ.get("AUDIO_THUMBNAIL", "")
+VIDEO_THUMBNAIL = environ.get("VIDEO_THUMBNAIL", "")
+UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL", "TGbotsXD")
