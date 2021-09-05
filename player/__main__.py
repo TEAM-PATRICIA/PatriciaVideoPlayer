65959cedecc002b2771cf31bc21318aae53472ba
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-09-05 13:42:37 +0530

    Create __main__.py

---
+++ player/__main__.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
+from player.modules.videoplayer import app
+
+
+player = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="modules"),
+)
+
+player.start()
+print("[INFO]: STARTING BOT CLIENT JOIN @TGBOTSXD || @TGBOTZXD")
+app.start()
+print("[INFO]: STARTING USERBOT CLIENT JOIN @TGBOTSXD || @TGBOTZXD")
+idle()
