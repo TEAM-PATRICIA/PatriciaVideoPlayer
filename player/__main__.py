bcf73c137fbcdbcd52755ec4545f80078dd29913
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-09-07 22:16:19 +0530

    Update __main__.py

--- player/__main__.py
+++ player/__main__.py
@@ -1,18 +1,48 @@
-from pyrogram import Client, idle
-from player.config import API_ID, API_HASH, BOT_TOKEN
-from player.modules.videoplayer import app
+import sys
+import glob
+import asyncio
+import importlib
+from pathlib import Path
+from pytgcalls import idle
+from player import Bot, UB, group_calls
+from player.config import Var
+from player.modules import player
+loop = asyncio.get_event_loop()
 
+_path = f"player/modules/*.py"
+files = glob.glob(_path)
 
-player = Client(
-    ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="modules"),
-)
+def load_plugins(plugin_name):
+    path = Path(f"player/modules/{plugin_name}.py")
+    name = "player.modules.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(load)
+    sys.modules[f"player.modules." + plugin_name] = load
+    print("Imported => " + plugin_name)
 
-player.start()
-print("[INFO]: STARTING BOT CLIENT JOIN @TGBOTSXD || @TGBOTZXD")
-app.start()
-print("[INFO]: STARTING USERBOT CLIENT JOIN @TGBOTSXD || @TGBOTZXD")
+async def client_start(bot=False):
+    print('\n')
+    print('------------------- Initalizing VC BOT ---------------------')
+    # Assistant bot
+    if bot:
+        await Bot.start()
+    await UB.start()
+    await group_calls.start()
+    print('----------------------- DONE ------------------------')
+    print('--------------------- Importing ---------------------')
+    for name in files:
+        with open(name) as a:
+            path_ = Path(a.name)
+            plugin_name = path_.stem
+            load_plugins(plugin_name.replace(".py", ""))
+    print('----------------------- INITIATED VIDEO PLAYER BOT ------------------------')
+    print('             Logged in as User =>> {}'.format((await UB.get_me()).first_name))
+    if bot:
+        print('             and Bot =>> {}'.format((await Bot.get_me()).first_name))
+    print('-----------------------------------------------------')
+
+if __name__ == '__main__':
+    is_bot = bool(Var.BOT_TOKEN)
+    loop.run_until_complete(client_start(bot=is_bot))
 idle()
