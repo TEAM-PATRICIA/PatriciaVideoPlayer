6348fad8c85fb22805b8d0a507d66c0775778370
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-09-07 22:14:12 +0530

    Update config.py

--- player/config.py
+++ player/config.py
@@ -1,18 +1,11 @@
-import os
-from os import path, getenv
+from os import getenv, environ
 from dotenv import load_dotenv
 
-if path.exists("local.env"):
-    load_dotenv("local.env")
-
 load_dotenv()
-admins = {}
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-SESSION_NAME = getenv("SESSION_NAME", None)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "")
-BOT_USERNAME = getenv("BOT_USERNAME", "")
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . ?").split())
+
+class Var(object):
+    API_ID = int(getenv('API_ID'))
+    API_HASH = str(getenv('API_HASH'))
+    BOT_TOKEN = getenv('BOT_TOKEN')
+    SESSION = str(getenv('SESSION'))
+    SUDO =  list(int(x) for x in getenv('SUDO', '').split())
